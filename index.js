cdffd5b1198ae40f3ec64e8216c8d62502760e02
Michael Jungo <michaeljungo92@gmail.com>
2017-06-27 22:19:39 +0200

    Add doc comments

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+/**
+ * Creates a new object which is the result of merging the two given objects.
+ * The objects are deeply merged unless the `option.overwrite` is specified.
+ * It differes from a usual deep merge, by automatically creating an array if
+ * the property is defined on both objects, and they aren't both objects
+ * themselves.
+ * For example: mergeObjects({ a: 1 }, { a: 2 }) === { a: [1, 2] }
+ * Most configs accept both, an item directly and an array of items. This makes
+ * it easier to combine them.
+ * This behaviour is disabled when the `option.overwrite` is specified.
+ *
+ * @param {Object} base The base config.
+ * @param {Object} extension The config that extends the base config.
+ * @param {Object} [options = {}]
+ * @param {Boolean} [options.overwrite] Overwrite existing properties instead of
+ * merging them.
+ * @returns {Object} The result of merging the base and extension config.
+ */
 function mergeObjects(base, extension, options = {}) {
   if (options.overwrite) {
     return Object.assign({}, base, extension);
@@ -27,6 +45,20 @@ function mergeObjects(base, extension, options = {}) {
   return merged;
 }
 
+/**
+ * Creates a config by merging the `common` config and the config(s) that match
+ * the current NODE_ENV. If no NODE_ENV is set, it defaults to `development`.
+ *
+ * @param {Object} config Each property is a config (object) which will be
+ * merged into the final config when the key matches NODE_ENV. Additionally the
+ * `common` property can be used, which will always be used, regardless of the
+ * NODE_ENV.
+ * @param {Object} [options = {}]
+ * @param {Boolean} [options.overwrite] Overwrite existing properties instead of
+ * merging them.
+ * @returns {Object} The config created by merging the common config with the
+ * config(s) of the current NODE_ENV.
+ */
 function configByEnv(config, options = {}) {
   const env = process.env.NODE_ENV || 'development';
   let finalConfig = {};
